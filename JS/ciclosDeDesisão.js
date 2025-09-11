4e56e55e06cadeb6e1186e8f8abba6c57585d3d0
Dario Quental <dhdq@hotmail.com>
2025-09-11 21:35:59 +0100

    add comments

--- JS/ciclosDeDesisão.js
+++ JS/ciclosDeDesisão.js
@@ -3,13 +3,13 @@ let num1=3;
 let num2=2;
 
 if (num1>num2){
-    console.log('maior é ',num1)
+    console.log('maior é ',num1);
 }
 else if(num1===num2){
-    console.log('Sao iguais')
+    console.log('Sao iguais');
 }
 else{
-    console.log('maior é ',num2)
+    console.log('maior é ',num2);
 }
 
 //switch
@@ -17,15 +17,15 @@ let opc =2;
 
 switch (opc){
     case 1:
-        console.log('esc 1')
+        console.log('esc 1');
     break;
     case 2:
-        console.log('esc 2')
+        console.log('esc 2');
     break;
     case 3:
-        console.log('esc 3')
+        console.log('esc 3');
     break;
     default:
-        console.log('Default')   
+        console.log('Default');   
     break;
 }
